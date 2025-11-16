83acbece7ce204f19c7e7c7d7d32ace00edc7983
zrs29 <zrs29@cornell.edu>
2025-11-16 16:24:50 -0500

    wavefront parallel

--- kernel.cpp
+++ kernel.cpp
@@ -7,6 +7,9 @@
 #define SEQUENCES  1
 #define SEQ_LENGTH 32
 
+#define QRY_CORE (SEQ_LENGTH / bsg_tiles_Y)
+#define REF_CORE (SEQ_LENGTH / bsg_tiles_X)
+
 // parameters
 #define NUM_TILES       (bsg_tiles_X*bsg_tiles_Y)
 #define MATCH     1
@@ -28,8 +31,11 @@ inline int max(int a, int b, int c, int d) {
   return max(max(a,b), max(c,d));
 }
 
-int maxv;
-int dp[5][33];
+int max_left = -1;
+int max_top  = -1;
+int maxv     = -1;
+
+int dp[QRY_CORE+1][REF_CORE+1];
 
 // Kernel main;
 extern "C" int kernel(uint8_t* qry, uint8_t* ref, int* output, int pod_id)
@@ -38,49 +44,75 @@ extern "C" int kernel(uint8_t* qry, uint8_t* ref, int* output, int pod_id)
   bsg_barrier_tile_group_sync();
   bsg_cuda_print_stat_kernel_start();
 
-  for (int i = 0; i <= 32; i++)
+  for (int i = 0; i <= REF_CORE; i++)
     dp[0][i] = 0;
 
-  for (int j = 0; j <= 4; j++)
+  for (int j = 0; j <= QRY_CORE; j++)
     dp[j][0] = 0;
 
-  maxv = 0;
+  int tmp;
 
-  if (__bsg_x == 0) {
-    // wait for core above to write
-    bsg_lr(&maxv);
-    if (__bsg_y) bsg_lr_aq(&maxv);
-    asm volatile("" ::: "memory");
-    
-    // do dp calculation
-    for (int i = 1; i <= 4; i++) {
-      for (int j = 1; j <= 32; j++) {
-        int match = (qry[__bsg_y * 4 + i - 1] == ref[j]) ? MATCH : MISMATCH;
+  // wait for core above to write
+  tmp = bsg_lr(&max_top);
+  if (__bsg_y && (tmp == -1)) bsg_lr_aq(&max_top);
+  asm volatile("" ::: "memory");
 
-        int score_diag = dp[i-1][j-1] + match;
-        int score_up   = dp[i-1][j]   - GAP;
-        int score_left = dp[i]  [j-1] - GAP;
+  // wait for core to the left to write
+  tmp = bsg_lr(&max_left);
+  if (__bsg_x && (tmp == -1)) bsg_lr_aq(&max_left);
+  asm volatile("" ::: "memory");
 
-        dp[i][j] = max(0, score_diag, score_up, score_left);
+  // derive maximum value
+  maxv = max(0, max_top, max_left);
+  
+  // do dp calculation
+  for (int i = 1; i <= QRY_CORE; i++) {
+    for (int j = 1; j <= REF_CORE; j++) {
+      int qi = (__bsg_y * QRY_CORE) + (i - 1);
+      int rj = (__bsg_x * REF_CORE) + (j - 1);
 
-        if (dp[i][j] > maxv) {
-          maxv = dp[i][j];
-        }
+      int match = (qry[qi] == ref[rj]) ? MATCH : MISMATCH;
+
+      int score_diag = dp[i-1][j-1] + match;
+      int score_up   = dp[i-1][j]   - GAP;
+      int score_left = dp[i]  [j-1] - GAP;
+
+      dp[i][j] = max(0, score_diag, score_up, score_left);
+
+      if (dp[i][j] > maxv) {
+        maxv = dp[i][j];
       }
     }
-
-    // copy over data to next core
-    if (__bsg_y < 7) {
-      int *ndp =  (int*) bsg_remote_ptr(__bsg_x, __bsg_y+1, &dp[0][0]);
-      for (int j = 1; j <= 32; j++) {
-        ndp[j] = dp[4][j];
-      }
-
-      // activate next core (and transfer max)
-      int *nmax =  (int*) bsg_remote_ptr(__bsg_x, __bsg_y+1, &maxv);
-      *nmax     = maxv;
-    } else *output = maxv;
   }
+
+  int *ndp, *nmax;
+
+  // copy data below
+  if (__bsg_y < (bsg_tiles_Y - 1)) {
+    ndp =  (int*) bsg_remote_ptr(__bsg_x, __bsg_y+1, &dp[0][0]);
+    for (int j = 0; j <= REF_CORE; j++) {
+      ndp[j] = dp[QRY_CORE][j];
+    }
+
+    // activate bottom core (and transfer max)
+    nmax =  (int*) bsg_remote_ptr(__bsg_x, __bsg_y+1, &max_top);
+    *nmax = maxv;
+  }
+
+  // copy data right
+  if (__bsg_x < (bsg_tiles_X - 1)) {
+    ndp = (int*) bsg_remote_ptr(__bsg_x+1, __bsg_y, &dp[0][0]);
+    for (int j = 0; j <= QRY_CORE; j++) {
+      ndp[(REF_CORE+1)*j] = dp[j][REF_CORE];
+    }
+
+    // activate right core (and transfer max)
+    nmax =  (int*) bsg_remote_ptr(__bsg_x+1, __bsg_y, &max_left);
+    *nmax = maxv;
+  }
+
+  if ((__bsg_x == (bsg_tiles_X - 1)) && (__bsg_y == (bsg_tiles_Y - 1)))
+    *output = maxv;
   
   // kernel end;
   bsg_fence();
